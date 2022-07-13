89ea28a2da3a5b8f2b2c7b808d430419d9bfb254
tetraminus <54343976+tetraminus@users.noreply.github.com>
2022-07-12 21:56:15 -0700

    add DMW

---
+++ Entities/DashMatchWall.cs
@@ -0,0 +1,50 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+
+// Celeste.ChaserBarrie
+namespace Celeste.Mod.TetraHelper.Entities
+
+{
+    
+	[CustomEntity("TetraHelper/DashMatchWall")]
+	[Tracked(false)]
+	public class DashMatchWall : Entity
+	{
+        private bool AllowGreater;
+        private int Amount;
+
+            private Vector2 control, end;
+
+        public DashMatchWall(EntityData data, Vector2 offset)
+            : this(data.Position + offset, data.Width, data.Height, data.Int("dashes", 1), data.Bool("GT", false)) { }
+
+            public DashMatchWall(Vector2 position, int width, int height, int dashes, bool greaterthan)
+                : base(position)
+            {
+               
+                
+                Depth = Depths.DreamBlocks - 1;
+                AllowGreater = greaterthan;
+                Amount = dashes;
+                Collider = new Hitbox(width, height);
+                Add(new PlayerCollider(OnPlayer));
+            }
+
+            private void OnPlayer(Player player)
+            {
+                if (!player.Dead && player.StateMachine.State != Player.StCassetteFly)
+                {
+                    if (AllowGreater)
+                    {
+                        if (!(player.Dashes == Amount)) { player.Die(Vector2.Zero); }
+                    }
+                    else
+                    {
+                        if (!(player.Dashes >= Amount)) { player.Die(Vector2.Zero); }
+                    }
+                }
+            }
+        }
+    }
